5d13d74dc6c2fbcede2678acc9d85761038d8d67
Keita Note <keitanote211@gmail.com>
2021-06-21 01:23:11 +0900

    modify json_encode

--- select.php
+++ select.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 // DB接続
 try {
     $pdo = new PDO('mysql:dbname=kadai_php02;charset=utf8;host=localhost','root','root'); // rootがデフォルト値
@@ -14,20 +12,19 @@ $stmt = $pdo->prepare('SELECT * FROM kadai_table_01') ;// SQLを準備
 $status = $stmt->execute(); 
 
 $view = '';
+$resultarray = []; 
 
 if ($status == false) {
     $error = $stmt->errorInfo();
   exit("ErrorQuery:".$error[2]);
 }else{
     // while($stmt ->fetch(PDO::FETCH_ASSOC)){
-    // $result = $stmt->fetchAll();(PDO::FETCH_ASSOC);    
-    // $json = json_encode($result);
+    // $result = $stmt->fetch(PDO::FETCH_ASSOC);    
+    // array_push($resultarray,$result);}
+    // $json = json_encode($result,SON_PRETTY_PRINT |JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     // }
     $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-    $json = json_encode($result);
-
-};
-
-
+    $json = json_encode($result,JSON_PRETTY_PRINT |JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);}
+    var_dump($json);
 
   ?>
